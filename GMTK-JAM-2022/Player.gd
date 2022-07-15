9d99c9be9a8ce7d56168d7e7d612e17d315affff
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2022-07-15 21:14:15 +0300

    Implemented basic player movement

---
+++ GMTK-JAM-2022/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody
+
+export var speed = 12
+
+var velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	direction.x += Input.get_axis("move_left" ,"move_right")
+	direction.z += Input.get_axis("move_forward", "move_back")
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+		
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
